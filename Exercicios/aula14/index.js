65a07e93d864a046d6ecda9cb4e4359eb859d13a
Mac <mac@MacBook-Pro-de-Mac.local>
2025-01-30 15:33:57 -0300

    Primeiro commit

---
+++ Exercicios/aula14/index.js
@@ -0,0 +1,11 @@
+//IEEE 754-2008
+let num1 = 10.25 //number
+let num2 = 2.5 //number
+
+//console.log(num1.toString() + num2);
+//num1 = num1.toString();
+//console.log(num1.toString(2));
+//console.log(num1.toFixed(4)); //numero inteiro
+//console.log(Number.isInteger(num1)); //retorna v ou f ou se e inteiro
+//let temp = num1 * '5';
+console.log(Number.isNan(temp));
